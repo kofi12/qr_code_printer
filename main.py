6b54ca112cab8aa4500a965bce630947877077b1
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2024-08-12 00:01:48 -0400

    add docker-compose.yml along with postgres volumes for persistance

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import SQLModel
+from models.db import engine
 from routes.qr_routes import qr_router
 from routes.auth_route import auth_router
 
 app = FastAPI()
+SQLModel.metadata.create_all(engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Adjust this as needed
--- models/db.py
+++ models/db.py
@@ -4,6 +4,7 @@ import os
 
 load_dotenv(find_dotenv())
 DATABASE_URL = os.getenv('DATABASE_URL', '')
+DOCKER_DB_URL = os.getenv('DOCKER_DB_URL', '')
 engine = create_engine(DATABASE_URL)
 
 def get_session():
